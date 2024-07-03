12d0d3e61bb8186728cb2e45eaac226da63d0bc4
Per <atlasfreak@gmx.de>
2024-07-04 01:04:44 +0200

    add javadoc

--- src/main/java/h01/template/Util.java
+++ src/main/java/h01/template/Util.java
@@ -1,38 +1,54 @@
 package h01.template;
 
+import java.util.Random;
+
 import fopbot.Direction;
 import fopbot.Robot;
 
-import java.util.Random;
-
+/**
+ * Utility class for the ghosts
+ */
 public class Util {
 
-	private static Random rnd = new Random();
+    private static Random rnd = new Random();
 
-	/**
-	 * @param min
-	 * @param max
-	 * @return a random integer between min and max (both inclusive)
-	 */
-	public static int getRandomInteger(int min, int max) {
-		return min + rnd.nextInt(max - min + 1);
-	}
+    /**
+     * Returns a random integer between min and max (both inclusive)
+     *
+     * @param min the minimum value
+     * @param max the maximum value
+     * @return a random integer between min and max (both inclusive)
+     */
+    public static int getRandomInteger(int min, int max) {
+        return min + rnd.nextInt(max - min + 1);
+    }
 
+    /**
+     * Returns the direction in which the robot is the furthest away from the chaser
+     *
+     * @param pacman the robot that is being chased
+     * @param chaser the robot that is chasing
+     * @return the direction in which the robot is the furthest away from the chaser
+     */
     public static Direction furthestDirection(Robot pacman, Robot chaser) {
         int px = pacman.getX();
         int py = pacman.getY();
         int cx = chaser.getX();
         int cy = chaser.getY();
 
-        int xdelta = Math.abs(px-cx);
-        int ydelta = Math.abs(py-cy);
+        int xdelta = Math.abs(px - cx);
+        int ydelta = Math.abs(py - cy);
 
         if (xdelta > ydelta) {
-            if (cx > px) return Direction.LEFT;
-            else return Direction.RIGHT;
+            if (cx > px)
+                return Direction.LEFT;
+            else
+                return Direction.RIGHT;
         } else {
-            if (cy > py) return Direction.DOWN;
-            else return Direction.UP;
+            if (cy > py)
+                return Direction.DOWN;
+            else
+                return Direction.UP;
         }
     }
 
